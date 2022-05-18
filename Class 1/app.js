d24d2b57df4f24d81398c42075a64677a330e229
Sabeer Waqas <71982222+Sabeerwaqas@users.noreply.github.com>
2022-05-18 08:52:57 +0500

    Add files via upload

---
+++ Class 1/app.js
@@ -0,0 +1,11 @@
+console.log("sabeer waqas");
+console.log(786)
+
+
+
+var userName  = "Saylani";
+console.log(userName);
+
+
+var userAge = prompt("Enter your age?");
+console.log("You are " + userAge + " years old");
